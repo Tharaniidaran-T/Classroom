c11aca67d2d6fd3e3e1ed031af403cc2b9b0b55d
peak diamond 2 <aedan.l.kennison@gmail.com>
2026-05-28 18:32:08 -0400

    Update games.ts

--- src/data/games.ts
+++ src/data/games.ts
@@ -239,7 +239,7 @@ export const games: Game[] = [
     id: 'game-54',
     title: 'Pokemon Black 2',
     description: 'yeah',
-    url: 'https://granddia2.github.io/pokemon/black2.html',
+    url: '/clpokeblack2.html;',
     thumbnail: 'https://www.nintendo.com/eu/media/images/10_share_images/games_15/nintendo_ds_22/SI_NDS_PokemonBlackAndWhite2_Black_enGB_image1600w.jpg',
     category: 'solo'
   },
